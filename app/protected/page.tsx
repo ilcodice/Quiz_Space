10648be56ef8a5353bf3d76bd349a5de20fd640f
Delshad Ibrahim <elcodius@gmail.com>
2025-07-25 16:12:39 +0200

    auth issue

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,9 +1,9 @@
-import { redirect } from "next/navigation";
-import react from "react";
-import { isAuthenticated } from "";
+// import { redirect } from "next/navigation";
+// import react from "react";
+// import { isAuthenticated } from "";
 
-export default async function page() {
-  let isAuth = await isAuthenticated();
-  if (!isAuth) return <div>Unauthorized</div>;
-  return <div>Protected Page</div>;
-}
+// export default async function page() {
+//   let isAuth = await isAuthenticated();
+//   if (!isAuth) return <div>Unauthorized</div>;
+//   return <div>Protected Page</div>;
+// }
