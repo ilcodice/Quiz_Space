d26050c79b53001cb2ffbb6677d8697ad0bd9b9c
Delshad Ibrahim <elcodius@gmail.com>
2025-07-25 16:25:18 +0200

    protected removed

--- app/protected/page.tsx
+++
@@ -1,9 +0,0 @@
-// import { redirect } from "next/navigation";
-// import react from "react";
-// import { isAuthenticated } from "";
-
-// export default async function page() {
-//   let isAuth = await isAuthenticated();
-//   if (!isAuth) return <div>Unauthorized</div>;
-//   return <div>Protected Page</div>;
-// }
